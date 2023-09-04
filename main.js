2308b61051c74fea6536d6c36cbab2347510aea9
devmarufurrahaman <mdmarufur5682@gmail.com>
2023-09-04 13:38:06 +0600

    done

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 let listArray = [];
 
 let checkBoxes = document.querySelectorAll(".checkbox");
+let formCheckList = document.getElementById("formCheckList");
+let submitBtn = document.getElementById("submitBtn");
+let myTable = document.getElementsByClassName("myTable");
+let tableRow = document.getElementById("tableRow");
+let data = document.getElementById("sd");
 
 for (let checkbox of checkBoxes) {
   checkbox.addEventListener("click", function (e) {
@@ -13,3 +18,23 @@ for (let checkbox of checkBoxes) {
     }
   });
 }
+
+function tableShow() {
+  for (let i = 0; i < listArray.length; i++) {
+    let th = document.createElement("th");
+
+    var cellText = document.createTextNode(listArray[i]);
+    th.appendChild(cellText);
+    tableRow.appendChild(th);
+  }
+}
+
+function dataShow() {
+  data.innerText = listArray.toString();
+}
+
+submitBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  tableShow();
+  dataShow();
+});
